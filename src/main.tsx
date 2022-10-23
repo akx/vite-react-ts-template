19c94ecf4a0d29ba51f73732028e6fb664a82376
Eetu Häivälä <eetu.haivala@gmail.com>
2022-10-23 13:23:43 +0300

    Use React 18 root element API
    ReactDOM.render is no longer supported in React 18. Update main.tsx to use
    createRoot instead.
    
    https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement!); // eslint-disable-line
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
+  </React.StrictMode>
 );
