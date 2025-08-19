320c4fda4ff765b7b0f2be221b281ed03006a014
Aarni Koskela <akx@iki.fi>
2025-08-19 14:04:54 +0300

    Bump dependencies + ESlint; tighten ESlint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,61 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const overrides = {
+  "unicorn/filename-case": "off",
+  "unicorn/no-null": "off",
+  "unicorn/prefer-optional-catch-binding": "off",
+  "unicorn/prevent-abbreviations": "off",
+  "unicorn/text-encoding-identifier-case": "off",
+  "react/no-unescaped-entities": "off",
+  "react/prop-types": "off",
+  "react/react-in-jsx-scope": "off",
+  "prefer-template": "error",
+  "@typescript-eslint/no-unused-vars": [
+    "error",
+    {
+      args: "all",
+      argsIgnorePattern: "^_",
+      caughtErrors: "all",
+      caughtErrorsIgnorePattern: "^_",
+      ignoreRestSiblings: false,
+      vars: "all",
+      varsIgnorePattern: "^_",
+    },
+  ],
+};
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "postcss.config.cjs"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: { react: { version: "detect" } },
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactHooks.configs["recommended-latest"],
+  eslintPluginUnicorn.configs.recommended,
+  prettierConfig,
+  {
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
-  prettierConfig,
+  {
+    rules: overrides,
+  },
 );
