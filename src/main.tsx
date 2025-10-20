3ed174e93271ea631327336ca4e21d83534978ad
Aarni Koskela <akx@iki.fi>
2025-10-20 21:22:01 +0300

    Bump dependencies + ESlint; tighten ESlint configuration (#4)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import "./index.css";
+
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
 
-const rootElement = document.getElementById("root");
+const rootElement = document.querySelector("#root");
 const root = createRoot(rootElement!);
 
 root.render(
